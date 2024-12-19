84d4d3d66fc9612d97fe90dc0782891186df11e9
Alex Goode <alexgoodestudio@gmail.com>
2024-12-19 15:37:37 -0500

    fixed env file

--- src/components/projects/Neatfreak/components/Login.js
+++ src/components/projects/Neatfreak/components/Login.js
@@ -2,17 +2,16 @@ import React, { useState } from "react";
 import EmployeeHome from "./EmployeeHome";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-
-const Password = process.env.REACT_APP_Password;
+const Password = process.env.REACT_APP_PASSWORD;
 
 const Login = () => {
   const [isVerified, setIsVerified] = useState(false);
+  const [inputPassword, setInputPassword] = useState("");
   const [error, setError] = useState("");
 
   const checkPw = (event) => {
-    event.preventDefault(); 
-    const answer = document.getElementById("password").value.trim();
-    if (answer === Password) {
+    event.preventDefault();
+    if (inputPassword === Password) {
       setIsVerified(true);
     } else {
       setError("Sorry, that's not it");
@@ -24,11 +23,25 @@ const Login = () => {
       {isVerified ? (
         <EmployeeHome />
       ) : (
-        <form onSubmit={checkPw} className="mt-5 d-flex align-items-center justify-content-center">
-          <label htmlFor="password" className=" me-3">Password: </label>
-          <input id="password" name="password" type="password" />
-          <button type="submit" className=" btn btn-outline-secondary ms- me-3">Login</button>
-          {error && <p className="">{error}</p>}
+        <form
+          onSubmit={checkPw}
+          className="mt-5 d-flex align-items-center justify-content-center"
+        >
+          <label htmlFor="password" className="me-3">
+            Password:
+          </label>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            value={inputPassword}
+            onChange={(e) => setInputPassword(e.target.value)}
+          />
+          <button type="submit" className="btn btn-outline-secondary ms-3">
+            Login
+          </button>
+          {error && <p className="text-danger ms-3">{error}</p>}
+
         </form>
       )}
     </>
