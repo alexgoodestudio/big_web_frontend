94df8b0dc10531538f76aedea288fff55949a3f9
Alex Goode <alexgoodestudio@gmail.com>
2024-12-20 17:52:07 -0500

    added github linkedin icons/ updated education section

--- src/portfoliolayout/PortlioLayout.js
+++ src/portfoliolayout/PortlioLayout.js
@@ -12,29 +12,30 @@ function PortfolioLayout() {
       <div className="row p-5">
         {/* Developer Description */}
         <div className="col-lg-6">
-
-
           <h1 className="text-white">Full-Stack Developer</h1>
+
           <br />
           <p className="justify-text text-white">
-            Hi, my name is Alex Goode. I am a Junior Software Engineer experienced in
-            Full Stack Development and DevOps with strong background in Customer
-            Service/ Graphic Design roles. Well-structured in approach to complex coding
-            problems. Completed Thinkful's (Chegg Skills) Full-Stack Software
-            Engineering Immersion Program, and Promineo Tech's Front-End
-            Software Development Certification Program. Passionate about keeping
-            up with latest tech and design trends. Flexible and open to
-            relocation for in-person, hybrid roles. US Citizen.
+            Hi, my name is <span className="bold">Alex Goode</span>. I am a
+            Junior <span className="underline">Software Engineer</span> in
+            Blythewood, South Carolina. Experienced in Full Stack Development
+            and DevOps with strong background in Customer Service/ Graphic
+            Design roles. I currently have 3 years of software development
+            experience. Throughout my education and professional experience
+            which can be found in my Resume linked beneath my portfolio picture,
+            I have become well-structured in approach to complex coding and
+            workplace challenges. Over the past few years, I have completed
+            Thinkful's (Chegg Skills) Full-Stack Software Engineering Immersion
+            Program, and Promineo Tech's Front-End Software Development
+            Certification Program. Passionate about keeping up with latest tech
+            and design trends. I am flexible and open to relocation for the
+            right in-person, or hybrid roles. US Citizen.
           </p>
         </div>
 
         {/* Image and Button Section */}
         <div className="col-lg-4 d-flex flex-column align-items-center justify-content-center">
-          <img
-            src={Img}
-            className="img-fluid w-50 rounded mb-4"
-            alt="img"
-          />
+          <img src={Img} className="img-fluid w-50 rounded mb-4" alt="img" />
           <a
             href="/resume/RESUME.pdf"
             download="RESUME.pdf"
