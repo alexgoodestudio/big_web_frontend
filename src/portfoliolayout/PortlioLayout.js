fa26433c60dfbf2d5c21d0e73f33b6a0e5e18026
Alex Goode <alexgoodestudio@gmail.com>
2024-12-21 18:46:28 -0500

    styling on dash

--- src/portfoliolayout/PortlioLayout.js
+++ src/portfoliolayout/PortlioLayout.js
@@ -4,29 +4,28 @@ import Img from "../img/IMG_5240.jpg";
 import NoProjectScreen from "./NoProjectScreen";
 import "../style.css";
 
-
 function PortfolioLayout() {
   const outlet = useOutlet();
 
   return (
-    <div className="bg-dark min-h-screen p-1">
-     
-      <div className="row py-5 px-3">
-        {/* Developer Description */}
-        <div className=" col-lg-1"></div>
-    
+    <div className="bg-dark min-h-screen p-1 ">
+
+      <div className="row py-5 px-3">      
+        <div className=" col-lg-1"></div>    
         <div className=" col-lg-6">
           <h1 className="text-white ">Full-Stack Developer</h1>
-
           <br />
           <p className="justify-text mobileText text-white">
             Hi, my name is <span className="bold">Alex Goode</span>. I am a
             Junior Freelance <span className="underline">Software Engineer</span> in
             Blythewood, South Carolina. Experienced in Full-Stack Development
             and DevOps with strong background in Customer Service/ Graphic
-            Design roles. I currently have 3 years of software development
+            Design roles.
+            <br/>
+            <br/>
+            I currently have 3 years of software development
             experience. Throughout my education and professional experience
-            which can be found in my Resume linked beneath my portfolio picture,
+            which conveniently can be found in my Resume linked beneath my portfolio picture,
             I have become well-structured in approach to complex coding and
             workplace challenges. Over the past few years, I have completed
             Chegg Skills (Thinkful) Full-Stack Software Engineering Immersion
@@ -39,7 +38,7 @@ function PortfolioLayout() {
 
         {/* Image and Button Section */}
         <div className="  col-lg-4 d-flex flex-column align-items-center justify-content-center">
-          <img src={Img} className="img-fluid fullWidth mobileWidth rounded mt-4 mb-4" alt="img" />
+          <img src={Img} className="img-fluid fullWidth mobileWidth rounded2 mt-4 mb-4" alt="img" />
           <a
             href="/resume/RESUME.pdf"
             download="RESUME.pdf"
