b017ec73e566d03be0ec66c0e5463fffcb41da3b
Alex Goode <alexgoodestudio@gmail.com>
2025-01-10 22:21:58 -0500

    updated tech stack

--- src/portfoliolayout/NoProjectScreen.js
+++ src/portfoliolayout/NoProjectScreen.js
@@ -7,6 +7,8 @@ import {
   FaHtml5,
   FaCss3Alt,
   FaBootstrap,
+  FaGithub,
+
 } from "react-icons/fa";
 import {
   SiExpress,
@@ -14,84 +16,103 @@ import {
   SiPostman,
   SiJest,
   SiMocha,
+  SiTailwindcss,
+  SiGreensock,
+  SiRender,
 } from "react-icons/si";
 import "./style.css";
 import Footer from "./Footer";
 
 function NoProjectScreen() {
   return (
-    <div className="bg-dark text-white p-3 ">
-        <h4 className="  mb-2  text-center bold">Current Tech Stack</h4>
-        <p className=" mb-5 mobileText text-center ">
-          I use these technologies thoughout my projects.
-        </p>
-      <div className=" px-5">
-        </div>
+    <div className="bg-dark text-white p-3">
+      <h4 className="mb-2 text-center bold">Current Tech Stack</h4>
+      <p className="mb-5 mobileText text-center">
+        I use these technologies throughout my projects.
+      </p>
       <div className="container">
         <div className="row text-center gy-4">
           {/* Node.js */}
-          <div className="col-4 col-md-2">
+          <div className="col-3 col-md-2">
             <FaNodeJs size={60} className="mb-2 text-success" />
             <h6 className="mobileText">Node.js</h6>
           </div>
           {/* Express */}
-          <div className="col-4 col-md-2">
+          <div className="col-3 col-md-2">
             <SiExpress size={60} className="mb-2 text-light" />
             <h6 className="mobileText">Express.js</h6>
           </div>
           {/* JavaScript */}
-          <div className="col-4 col-md-2">
+          <div className="col-3 col-md-2">
             <FaJs size={60} className="mb-2 text-warning" />
             <h6 className="mobileText">JavaScript</h6>
           </div>
           {/* React */}
-          <div className="col-4 col-md-2">
+          <div className="col-3 col-md-2">
             <FaReact size={60} className="mb-2 text-info" />
             <h6 className="mobileText">React</h6>
           </div>
           {/* PostgreSQL */}
-          <div className="col-4 col-md-2">
+          <div className="col-3 col-md-2">
             <SiPostgresql size={60} className="mb-2 text-primary" />
             <h6 className="mobileText">PostgreSQL</h6>
           </div>
           {/* SQL */}
-          <div className="col-4 col-md-2">
+          <div className="col-3 col-md-2">
             <FaDatabase size={60} className="mb-2 text-secondary" />
             <h6 className="mobileText">SQL</h6>
           </div>
           {/* HTML5 */}
-          <div className="col-4 col-md-2">
+          <div className="col-3 col-md-2">
             <FaHtml5 size={60} className="mb-2 text-danger" />
             <h6 className="mobileText">HTML5</h6>
           </div>
           {/* CSS3 */}
-          <div className="col-4 col-md-2">
+          <div className="col-3 col-md-2">
             <FaCss3Alt size={60} className="mb-2 text-primary" />
             <h6 className="mobileText">CSS3</h6>
           </div>
           {/* Bootstrap */}
-          <div className="col-4 col-md-2">
+          <div className="col-3 col-md-2">
             <FaBootstrap size={60} className="mb-2 text-info" />
             <h6 className="mobileText">Bootstrap</h6>
           </div>
           {/* Postman */}
-          <div className="col-4 col-md-2">
+          <div className="col-3 col-md-2">
             <SiPostman size={60} className="mb-2 text-danger" />
             <h6 className="mobileText">Postman</h6>
           </div>
           {/* Jest */}
-          <div className="col-4 col-md-2">
+          <div className="col-3 col-md-2">
             <SiJest size={60} className="mb-2 text-success" />
             <h6 className="mobileText">Jest</h6>
           </div>
           {/* Mocha */}
-          <div className="col-4 col-md-2">
+          <div className="col-3 col-md-2">
             <SiMocha size={60} className="mb-2 text-warning" />
             <h6 className="mobileText">Mocha</h6>
           </div>
+          {/* Tailwind CSS */}
+          <div className="col-3 col-md-2">
+            <SiTailwindcss size={60} className="mb-2 text-cyan-400" />
+            <h6 className="mobileText">Tailwind CSS</h6>
+          </div>
+          {/* GSAP */}
+          <div className="col-3 col-md-2">
+            <SiGreensock size={60} className="mb-2 text-green-400" />
+            <h6 className="mobileText">GSAP</h6>
+          </div>
+          <div className="col-3 col-md-2">
+            <FaGithub size={60} className="mb-2 text-green-400" />
+            <h6 className="mobileText">Github</h6>
+          </div>
+          <div className="col-3 col-md-2">
+            <SiRender size={60} className="mb-2 text-green-400" />
+            <h6 className="mobileText">Render</h6>
+          </div>
         </div>
       </div>
-  
+
       <Footer className="text-white" />
     </div>
   );
